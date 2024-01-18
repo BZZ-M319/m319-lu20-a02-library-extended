5975c3a3ca16aad0901ec6632d2498d7491fb3ed
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-01-18 08:50:00 +0100

    made ready

--- library.py
+++ library.py
@@ -1,25 +1,68 @@
-def init_books():
-    pass
+from rental import Rental
+from datetime import datetime
 
+
+def init_books():
+    #TODO: Load book.json and return as Dictionary
+    return None
 
 def read_rental():
-    pass
+    rental_date = read_date('Enter rental date (dd.mm.yyyy): ')
+    num_rental_days = read_int('Enter number of rental days: ', 1, 99)
+    return_date = read_date('Enter return date (dd.mm.yyyy): ')
+    return Rental(rental_date, return_date, num_rental_days)
 
 
 def add_rental(books):
-    print('Here is the book rental part')
+    while True:
+        book_name = input('Enter the book name: ')
+        if book_name in books:
+            rental = read_rental()
+            books[book_name].append(rental)
+            choice = input('Do you want to add another rental? (y/n): ').strip().lower()
+            if choice != 'y':
+                break
+    else:
+        print('Book not found in the library.')
 
 
 def show_balance(books):
-    print('Print the book balance here')
+    for book, rentals in books.items():
+        print(f'Statement for {book}')
+        total_cost = 0
+
+        for rental in rentals:
+            print(f'  - {rental.rental_date.strftime("%d.%m.%Y")}: CHF {rental.cost}')
+            total_cost += rental.cost
+
+        print(f'Total: CHF {round(total_cost, 2)}')
 
 
-def read_int(prompt, minimum, maximum):
-    return int(input(prompt))  # Placeholder to let main run. Delete when doing the aassignment
+def read_int(prompt, minimum=None, maximum=None):
+    while True:
+        try:
+            num = int(input(prompt))
+        except ValueError:
+            print('Please, enter a whole number!')
+            continue
+        else:
+            if minimum is not None and num < minimum:
+                print('Please, enter a number greater than or equal to', minimum)
+                continue
+            if maximum is not None and num > maximum:
+                print('Please, enter a number less than or equal to', maximum)
+                continue
+            return num
 
 
 def read_date(prompt):
-    pass
+    while True:
+        try:
+            date_str = input(prompt)
+            rental_date = datetime.strptime(date_str, '%d.%m.%Y')
+            return rental_date
+        except ValueError:
+            print('Please enter a valid date.')
 
 
 def main():
